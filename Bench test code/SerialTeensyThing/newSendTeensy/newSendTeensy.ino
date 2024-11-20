04f5451abb5224f45a724a1b09223be6cd2e7b50
Aaron Jung <jjung999@terpmail.umd.edu>
2024-11-20 18:20:02 -0500

    serial connection code

---
+++ Bench test code/SerialTeensyThing/newSendTeensy/newSendTeensy.ino
@@ -0,0 +1,34 @@
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.println("I am sender teensy");
+}
+
+void loop() {
+  //Serial1.write("Hello receiverTeensy");
+  String message = "Hello receiverTeensy";
+  // creating a char[] to write back
+    int length = message.length();
+    char str[length];
+    for (int i = 0; i < length; i++) {
+      str[i] = message.charAt(i);
+    }
+    Serial1.write(str, length);
+
+  //does nothing until data is detected
+  while (Serial1.available() == 0) {
+    Serial.println("Waiting for data");
+    delay(1000);
+  }
+  readAndSpit();
+}
+
+void readAndSpit() {
+//Serial.available will return an int if it has received data
+  if (Serial1.available() > 0) {
+    Serial.println("Received!");
+    String message = Serial1.readStringUntil('\n');  // Read the incoming message
+    Serial.print("Message from SenderTeensy: ");
+    Serial.println(message);
+  }
+}
