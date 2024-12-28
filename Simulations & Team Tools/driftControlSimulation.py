a46139418e2f4efd80186242ad9390c9f4dc5ac8
ChenJeff123 <143658756+ChenJeff123@users.noreply.github.com>
2024-12-28 12:17:03 -0500

    added simulations of different estimations of vehicle speed

--- Simulations & Team Tools/driftControlSimulation.py
+++ Simulations & Team Tools/driftControlSimulation.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+import time
 
 # Define constants
 K = 2  # Motor constant
@@ -14,6 +15,19 @@ RAND = 10/DT
 
 target = (0,5)
 
+class WindCorrector():
+  def __init__(self, pos, percentVS):
+    self.pos = copy.deepcopy(pos)
+    self.heading = [[0] for _ in pos]
+    self.xvel = [[0] for _ in pos]
+    self.yvel = [[0] for _ in pos]
+    self.total_time = [1] * len(pos)
+    self.ang_vel = [0] * len(pos)
+    self.past_error = [0] * len(pos)
+    self.completed = []
+    self.prev_goal = [-1] * len(pos)
+    self.percentVS = percentVS
+
 # Function to generate wind vector in polar form
 def generate_wind(a, b, c, d, e, f, wind_mag):
   wind_mag_change = (a * random.uniform(-1, 1) + b) / DT
@@ -24,7 +38,7 @@ def generate_wind(a, b, c, d, e, f, wind_mag):
   return wind_mag, wind_angle
 
 # Function to calculate goal angle
-def calculate_goal_angle(pos, heading, corrected):
+def calculate_goal_angle(pos, heading, prev_goal, goal_index, percentVS):
   global target
 
   # distance from current to target
@@ -34,10 +48,10 @@ def calculate_goal_angle(pos, heading, corrected):
   return_angle = math.degrees(math.atan2(dy, dx)) % 360
 
   # wind correction logic
-  if corrected:
+  if prev_goal != None:
     wind = np.array([0,0])
     if len(pos[0]) > 1:
-      wind = np.array([pos[0][-1]-pos[0][-2], pos[1][-1]-pos[1][-2]]) - VS*DT*np.array([math.cos(math.radians(heading[-1])), math.sin(math.radians(heading[-1]))])
+      wind = np.array([pos[0][-1]-pos[0][-2], pos[1][-1]-pos[1][-2]]) - percentVS * VS*DT*np.array([math.cos(math.radians(heading[-1])), math.sin(math.radians(heading[-1]))])
       wind /= DT
     
     direction = np.array([dx, dy])
@@ -46,20 +60,25 @@ def calculate_goal_angle(pos, heading, corrected):
     wind_todirection = direction * np.dot(wind, direction) / direction_norm**2
     wind_cross = wind - wind_todirection
 
-    h_prime = direction/direction_norm * np.sqrt(VS**2 - np.linalg.norm(wind_cross)**2)
+    h_prime = direction/direction_norm * np.sqrt((percentVS*VS)**2 - np.linalg.norm(wind_cross)**2)
 
     goal_vec = h_prime - wind_cross
 
     goal_angle = math.degrees(math.atan2(goal_vec[1], goal_vec[0])) % 360
 
+    if prev_goal[goal_index] != -1:
+      goal_angle = 0.2 * goal_angle + 0.8 * prev_goal[goal_index]
+
     return_angle = goal_angle
+
+    prev_goal[goal_index] = return_angle
 
   return return_angle
 
 # Function simulates PD controller
-def system(pos, heading, past_error, corrected):
+def system(pos, heading, past_error, prev_goal, goal_index, percentVS):
   # Calculate goal angle
-  goal_angle = calculate_goal_angle(pos, heading, corrected)
+  goal_angle = calculate_goal_angle(pos, heading, prev_goal, goal_index, percentVS)
 
   # Calculate error terms
   angle_error = goal_angle - heading[-1]
@@ -80,7 +99,7 @@ def update_state(ang_vel, index, pwm):
   return ang_vel[index] * DT
 
 # updates the heading and position
-def move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle, corrected):
+def move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle, prev_goal = None, percentVS = 1):
   global target
 
   # iterates through each starting point
@@ -88,7 +107,7 @@ def move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, w
     if i not in completed:
       total_time[i] += 1
       # Get system output
-      pwm, past_error[i] = system(pos[i], heading[i], past_error[i], corrected)
+      pwm, past_error[i] = system(pos[i], heading[i], past_error[i], prev_goal, i, percentVS)
 
       # Update state
       heading[i].append(heading[i][-1] + update_state(ang_vel, i, pwm))
@@ -116,14 +135,11 @@ def simulate(pos, steps, rand_mag, offset, wind_mag_lower, wind_mag_upper, ang_l
   completed = []
 
   # wind-corrected state
-  posw = copy.deepcopy(pos)
-  headingw = copy.deepcopy(heading)
-  xvelw = copy.deepcopy(xvel)
-  yvelw = copy.deepcopy(yvel)
-  total_timew = [1] * len(pos)
-  ang_velw = [0] * len(pos)
-  past_errorw = [0] * len(pos)
-  completedw = []
+  versions = []
+  # different estimated values of the vehicle speed
+  percentVS = [0.25, 0.33, 0.5, 1, 2, 3, 4]
+  for percent in percentVS:
+    versions.append(WindCorrector(pos, percent))
       
   # Simulation loop
   for _ in range(int(steps)):
@@ -131,40 +147,55 @@ def simulate(pos, steps, rand_mag, offset, wind_mag_lower, wind_mag_upper, ang_l
     wind_mag, wind_angle = generate_wind(rand_mag, offset, wind_mag_lower, wind_mag_upper, ang_lower, ang_upper, wind_mag)
     
     # the non-wind-corrected simulation
-    move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle, False)
+    move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle)
 
     # wind-corrected simulation
-    move(posw, headingw, total_timew, ang_velw, xvelw, yvelw, past_errorw, completedw, wind_mag, wind_angle, True)
-      
-  # displays all graphs for each starting point
+    for state in versions:
+      move(state.pos, state.heading, state.total_time, state.ang_vel, state.xvel, state.yvel, state.past_error, state.completed, wind_mag, wind_angle, state.prev_goal, state.percentVS)
+  
+  cmap = plt.get_cmap('tab10', len(versions))
+
+  # displays all graphs for each starting point and wind estimation
   for i in range(len(pos)):
     plt.figure()
     plt.title(f"Position of Starting Point ({pos[i][0][0]}, {pos[i][1][0]})")
     plt.plot(pos[i][0], pos[i][1], ls='-', color='pink', label='non-corrected')
-    plt.plot(posw[i][0], posw[i][1], ls='-', color='red', label='wind corrected')
-    plt.legend()
+    
+    for j,state in enumerate(versions):
+      descriptor = f"wind corrected ({state.percentVS}x of actual vehicle speed)"
+      plt.plot(state.pos[i][0], state.pos[i][1], ls='-', color=cmap(j), label=descriptor)
+    plt.legend(fontsize='small')
 
     plt.figure()
     plt.subplot(2, 1, 1) 
     plt.title("X Velocity")
-    plt.plot(np.arange(0, (total_time[i])*DT, DT), xvel[i], ls='-', color='orange', label='non-corrected')
-    plt.plot(np.arange(0, (total_timew[i])*DT, DT), xvelw[i], ls='-', color='brown', label='wind corrected')
-    plt.legend()
+    plt.plot(np.arange(0, (total_time[i])*DT, DT), xvel[i], ls='-', color='pink', label='non-corrected')
+
+    for j,state in enumerate(versions):
+      descriptor = f"wind corrected ({state.percentVS}x of actual vehicle speed)"
+      plt.plot(np.arange(0, (state.total_time[i])*DT, DT), state.xvel[i], ls='-', color=cmap(j), label=descriptor)
+    plt.legend(fontsize='small')
 
     plt.subplot(2, 1, 2) 
     plt.title("Y Velocity")
-    plt.plot(np.arange(0, (total_time[i])*DT, DT), yvel[i], ls='-', color='olive', label='non-corrected')
-    plt.plot(np.arange(0, (total_timew[i])*DT, DT), yvelw[i], ls='-', color='green', label='wind corrected')
+    plt.plot(np.arange(0, (total_time[i])*DT, DT), yvel[i], ls='-', color='pink', label='non-corrected')
+
+    for j,state in enumerate(versions):
+      descriptor = f"wind corrected ({state.percentVS}x of actual vehicle speed)"
+      plt.plot(np.arange(0, (state.total_time[i])*DT, DT), state.yvel[i], ls='-', color=cmap(j), label=descriptor)
     plt.tight_layout()
-    plt.legend()
+    plt.legend(fontsize='small')
 
     plt.figure()
     plt.title(f"Heading of Starting Point ({pos[i][0][0]}, {pos[i][1][0]})")
-    plt.plot(np.arange(0,(total_time[i])*DT,DT), heading[i], ls = '-', color='cyan', label='non-corrected')
-    plt.plot(np.arange(0,(total_timew[i])*DT,DT), headingw[i], ls = '-', color='blue', label='wind corrected')
-    plt.legend()
-    plt.show()
+    plt.plot(np.arange(0,(total_time[i])*DT,DT), heading[i], ls = '-', color='pink', label='non-corrected')
 
+    for j,state in enumerate(versions):
+      descriptor = f"wind corrected ({state.percentVS}x of actual vehicle speed)"
+      plt.plot(np.arange(0,(state.total_time[i])*DT,DT), state.heading[i], ls = '-', color=cmap(j), label=descriptor)
+    
+    plt.legend(fontsize='small')
+    plt.show()
 
 # wind variables
 rand_mag = 0.1
